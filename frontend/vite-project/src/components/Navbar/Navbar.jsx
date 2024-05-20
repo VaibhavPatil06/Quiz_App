8258c73f6021e79ad400390cdd0f0844968868d5
VaibhavPatil06 <152978083+VaibhavPatil06@users.noreply.github.com>
2024-05-20 18:47:31 +0530

    Update Navbar.jsx

--- frontend/vite-project/src/components/Navbar/Navbar.jsx
+++ frontend/vite-project/src/components/Navbar/Navbar.jsx
@@ -1,20 +1,18 @@
 import React from "react";
 
 const Navbar = ({ setShowLogin, login, setLogin, setToken }) => {
+  const handleLogout = () => {
+    localStorage.removeItem("authToken");
+    setShowLogin(false);
+    setLogin();
+    setToken();
+  };
   return (
     <nav className="navbar">
       <div className="navbar-logo">Quiz App</div>
       <div className="navbar-links">
         {login ? (
-          <button
-            onClick={() => {
-              setShowLogin(false);
-              setLogin();
-              setToken();
-            }}
-          >
-            LogOut
-          </button>
+          <button onClick={handleLogout}>LogOut</button>
         ) : (
           <button onClick={() => setShowLogin(true)}>Login</button>
         )}
